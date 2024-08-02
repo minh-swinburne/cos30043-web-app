ee4d94d851e09f3c41b665682d52ccf9320253ea
Minh Nguyen <104169617@student.swin.edu.au>
2024-08-02 17:00:11 +0700

    add action buttons to register view

--- frontend/src/views/Authentication/RegisterView.vue
+++ frontend/src/views/Authentication/RegisterView.vue
@@ -43,50 +43,93 @@
             Register
           </v-card-title>
 
-          <v-card-text>
-            <v-form @submit.prevent="register">
+          <v-card-text class="pb-0">
+            <v-form ref="form" @submit.prevent="register">
               <v-text-field
                 v-model="username"
+                :rules="rules.usernameRules"
                 :prepend-icon="mdiAccount"
                 label="Username"
                 type="text"
                 color="primary"
                 autocomplete="username"
-                required
+                class="mb-5"
+                required counter
               ></v-text-field>
 
               <v-text-field
                 v-model="pwd1"
+                :rules="rules.pwdRules" 
                 :prepend-icon="mdiLock"
                 :append-inner-icon="showPwd1 ? mdiEye : mdiEyeOff"
                 :type="showPwd1 ? 'text' : 'password'"
                 label="Password"
                 color="primary"
                 autocomplete="new-password"
-                required
+                class="mb-5"
+                required counter
                 @click:append-inner="showPwd1 = !showPwd1"
+                @copy.prevent @cut.prevent
               ></v-text-field>
 
               <v-text-field
                 v-model="pwd2"
+                :rules="pwd2Rules"
                 :prepend-icon="mdiLock"
                 :append-inner-icon="showPwd2 ? mdiEye : mdiEyeOff"
                 :type="showPwd2 ? 'text' : 'password'"
                 label="Confirm Password"
                 color="primary"
                 autocomplete="new-password"
-                required
+                class="mb-5"
+                required counter
                 @click:append-inner="showPwd2 = !showPwd2"
+                @copy.prevent @cut.prevent
               ></v-text-field>
-
-              <v-btn
-                color="primary"
-                class="d-block mx-auto"
-              >
-                Next
-              </v-btn>
             </v-form>
           </v-card-text>
+
+          <v-card-actions class="px-5 px-xs-2 py-0">
+            <v-spacer></v-spacer>
+
+            <v-tooltip text="Reset Form" location="top">
+              <template v-slot:activator="{ props }">
+                <v-btn 
+                  v-bind="props" 
+                  color="red" 
+                  variant="elevated"
+                  :icon="mdiRefresh" 
+                  @click="reset()"
+                ></v-btn>
+              </template>
+            </v-tooltip>
+
+            <v-tooltip text="Reset Validation" location="top">
+              <template v-slot:activator="{ props }">
+                <v-btn 
+                  v-bind="props" 
+                  color="orange-darken-1"
+                  variant="elevated"
+                  :icon="mdiBroom" 
+                  @click="resetValidation()"
+                ></v-btn>
+              </template>
+            </v-tooltip>
+          </v-card-actions>
+          
+          <v-card-actions class="mb-5 px-5 px-xs-2">
+            <v-spacer></v-spacer>
+            <v-btn 
+              color="blue"
+              variant="elevated"
+              @click="nextFieldset()" 
+              :loading="loading"
+              :append-icon="mdiArrowRight"
+            >
+              Next
+            </v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
         </v-card>
 
         <v-row no-gutters class="pa-1 mt-5 align-center flex-0-1">
@@ -104,20 +147,47 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useAuthStore } from '@/stores'
-import { mdiAccount, mdiLock, mdiEye, mdiEyeOff } from '@mdi/js'
+import { mdiAccount, mdiLock, mdiEye, mdiEyeOff, mdiRefresh, mdiBroom, mdiArrowRight } from '@mdi/js'
 
 import BackButton from '@/components/buttons/ButtonBack.vue'
 
 const authStore = useAuthStore()
 
+const form = ref(null)
 const username = ref('')
 const pwd1 = ref('')
 const pwd2 = ref('')
 
 const showPwd1 = ref(false)
 const showPwd2 = ref(false)
+
+const rules = ref({
+  usernameRules: [
+		  v => !!v || 'Username is required',
+			v => v.length >= 3 || 'Username must be at least 3 characters',
+		],
+
+  pwdRules: [
+    v => !!v || 'Password is required',
+    v => v.length >= 8 || 'Password must be at least 8 characters',
+    v => /[\$\%\^\&\*]/.test(v) || 'Password must contain at least one special character ($, %, ^, &, *)',
+  ],
+})
+
+const pwd2Rules = computed(() => [
+  v => !!v || 'Confirm password is required',
+  v => v === pwd1.value || 'Passwords must match',
+])
+
+function reset() {
+  form.value.reset()
+}
+
+function resetValidation() {
+  form.value.resetValidation()
+}
 
 function nextPage() {
   console.log('Step 1')
